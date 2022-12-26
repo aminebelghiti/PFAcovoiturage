9ce32d21da1a7028f98674e4ffe6705a1721bb7a
Mohammed Amine <aminebelghiti9@gmail.com>
2022-12-26 19:55:14 +0100

    Initial commit

---
+++ src/main/java/ma/emsi/covoiturage/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ma.emsi.covoiturage;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CovoiturageApplication.class);
+    }
+
+}
---
+++ src/test/java/ma/emsi/covoiturage/CovoiturageApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.covoiturage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovoiturageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
